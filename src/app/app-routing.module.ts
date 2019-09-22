fb4c4a155212b790be62419ce5bdf4eb77a0ed47
Leila <leilalendiy@users.noreply.github.com>
2019-09-22 00:22:16 -0400

    Add home, signup, login, and update form pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { UpdateComponent } from './update/update.component';
 
 const routes: Routes = [
   {path: '', component: HomepageComponent},
   {path: 'home', component: HomepageComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'update', component: UpdateComponent},
   {path: '**', redirectTo: '/'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { UpdateComponent } from './update/update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    SignupComponent,
+    LoginComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
